9032ef4e9db9fd0fe48aed47ebbbc72e7ff48cd8
Miguel Rodriguez <miglos@hotmail.es>
2016-04-18 23:40:53 +0200

    Modified static template.

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,9 @@
+    <!-- Main Footer -->
+    <footer class="main-footer">
+        <!-- To the right -->
+        <div class="pull-right hidden-xs">
+            Small scripts and tests.
+        </div>
+        <!-- Default to the left -->
+        <strong>Copyright &copy; 2016 <a href="#">codde.it</a></strong>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;All rights reserved.
+    </footer>
