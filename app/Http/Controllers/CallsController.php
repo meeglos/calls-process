4a705b71255a2b97915b3d4f8b3c54498b2a13a5
Miguel Rodriguez <miglos@hotmail.es>
2016-05-09 03:07:28 +0200

    Inserting arrays done

--- app/Http/Controllers/CallsController.php
+++ app/Http/Controllers/CallsController.php
@@ -12,7 +12,7 @@ class CallsController extends Controller
 {
     public function latest()
     {
-        $calls = Call::orderBy('call_date', 'DESC')->paginate(10);
+        $calls = Call::orderBy('call_date', 'DESC')->paginate(20);
 
         return view('calls/list', compact('calls'));
     }
@@ -44,12 +44,12 @@ class CallsController extends Controller
             $mylog = $_POST['details'];
             $input = preg_replace("/((\r?\n)|(\r\n?))/", '**', $mylog);
             $pieces = explode('**', $input);
-            $fields = array('call_date', 'client_id', 'call_lapse', 'comment');
+            $fields = array('call_date', 'client_id', 'call_lapse', 'user_id', 'comment');
 
             foreach ($pieces as $part) {
 
                 $a = explode(' ', $part);
-                $b = array(2, 1, 1);
+                $b = array(2, 1, 1, 1);
                 $i = 0;
                 unset($output);
 
@@ -63,22 +63,12 @@ class CallsController extends Controller
                 $result[] = array_combine($fields, $output);
             }
 
-            $sqlSt = "INSERT INTO `" . $table . "`(" . implode(",", $fields) . ") VALUES ";
-
-            foreach ($result as $row) {
-                $sqlSt .= "('" . $row['call_date'] . "', ";
-                $sqlSt .= $row['client_id'] . ", ";
-                $sqlSt .= $row['call_lapse'] . ", ";
-                $sqlSt .= $row['comment'] . ", '1'),";
-            }
-            $sqlSt = rtrim($sqlSt,-1);
-            dd($sqlSt);
-
-            Call::create($sqlSt);
+            DB::table('calls')->insert($result);
 
             return Redirect::route('calls.latest');
         }
     }
+
     public function myconcat($carry, $item)
     {
         $carry .= $item . ' ';
